f8d1de6b4810a56b212638014883bce0626e248b
Kalgus <Kalgus@PC290.stokab.prod>
2014-11-20 10:51:01 +0100

    ignore

--- degreeWork/src/Model/DatasetCreator.java
+++ degreeWork/src/Model/DatasetCreator.java
@@ -16,7 +16,7 @@ import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 
 public class DatasetCreator {
-
+    
     private DatasetCreator() throws BiffException, WriteException, IOException {
     }
 
@@ -123,7 +123,7 @@ public class DatasetCreator {
         int year;
         int number;
         Calendar cal = Calendar.getInstance();
-        List daySeen = new ArrayList();
+        List daysSeen = new ArrayList();
         Day dayRead;
         for (int i = 1; i < rowsToCheck; i++) {
 
@@ -134,25 +134,8 @@ public class DatasetCreator {
             month = cal.get(Calendar.MONTH) + 1;
             year = cal.get(Calendar.YEAR);
             dayRead = new Day(day, month, year);
-            daySeen.add(dayRead);
-            switch (i) {
-                case 2000:
-                    System.out.println("Progress: " + 10 + "%");
-                    break;
-                case 6000:
-                    System.out.println("Progress: " + 30 + "%");
-                    break;
-                case 10000:
-                    System.out.println("Progress: " + 50 + "%");
-                    break;
-                case 15000:
-                    System.out.println("Progress: " + 75 + "%");
-                    break;
-                case 18000:
-                    System.out.println("Progress: " + 90 + "%");
-                    break;
-            }
-            number = countNumberEqual(daySeen, dayRead);
+            daysSeen.add(dayRead);
+            number = countNumberEqual(daysSeen, dayRead);
             series.addOrUpdate(dayRead, number);
         }
 
